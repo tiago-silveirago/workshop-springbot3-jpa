dd1d2cf740b7097a6f4f2de36612a7edbc3a1f66
Tiago Silveira <tiago.silveirago@gmail.com>
2025-07-16 11:43:43 -0300

    Category

--- src/main/java/com/educandoweb/course/services/OrderService.java
+++ src/main/java/com/educandoweb/course/services/OrderService.java
@@ -1,7 +1,6 @@
 package com.educandoweb.course.services;
 
 import com.educandoweb.course.entities.Order;
-import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
